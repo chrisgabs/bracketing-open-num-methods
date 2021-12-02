8de5578918a6f002b9d28eb8fc54ab045ab3392c
Christian Bayquen <cgabayquen@gmail.com>
2021-12-02 10:02:52 +0800

    add: bisection now functional

--- server.py
+++ server.py
@@ -2,8 +2,21 @@ import sys
 from flask import Flask, render_template, request, redirect, Response
 import random, json
 
+from numpy import number
+from bisection import bisection
+
 
 app = Flask(__name__)
+
+
+@app.route('/receiver', methods = ['POST'])
+def compute():
+	data = json.loads(request.get_data())
+	eq = data['eq']
+	xl = float(data['xl'])
+	xu = float(data['xu'])
+	crit = float(data['crit'])
+	return bisection(eq, xl, xu, crit)
 
 
 @app.route('/')
--- static/script/script.js
+++ static/script/script.js
@@ -1,3 +1,24 @@
 $(function() {
-    alert("shee")
+
+    $("#calculate").on("click", () => onCalculate())
+    
 })
+
+function onCalculate() {
+    const bisection = ["eq", "xu", "xl", "crit"]
+    let data = new Object()
+
+    bisection.forEach(e => {
+        const id = "#" + e
+        data[e] = $(id).val()
+    })
+    // const eq = $("#eq").val()
+    // const xu = $("#xu").val()
+    // const xl = $("#xl").val()
+    // const crit = $("#crit").val()
+    data = JSON.stringify(data)
+    console.log(data)
+    $.post("receiver", data, function(response) {
+        $("#output-container").append(response)
+    })
+}
